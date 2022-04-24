1b601f753b30a55b59f6b969860db47c5f53e1a5
Brenainn Vanhaverbeke <brenainnvanhaverbeke@gmail.com>
2022-04-24 23:19:51 +0200

    Initial push.

---
+++ Source/ExamTemplate/Creator.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BP"
+
+/**
+ * 
+ */
+class EXAMTEMPLATE_API Creator
+{
+public:
+	Creator();
+	~Creator();
+
+	static void CreateDrugBaseInstance(BP_DrugTypes drugType)
+	{
+		
+	}
+};
